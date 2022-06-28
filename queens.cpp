1c6b7c1b7e13f91edfc3f60ceccd8d4712706374
dkulemin <blablamaildk@gmail.com>
2022-06-28 11:06:40 +0500

    queens

---
+++ queens.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
+int n;
+int cnt = 0;
+int *col, *diag1, *diag2;
+
+void search(int y) {
+    if (y == n) {
+        cnt++;
+        return;
+    }
+    for (int x = 0; x < n; x++) {
+        if (col[x] || diag1[x + y] || diag2[x - y + n - 1]) continue;
+        col[x] = diag1[x + y] = diag2[x - y + n - 1] = 1;
+        search(y + 1);
+        col[x] = diag1[x + y] = diag2[x - y + n - 1] = 0;
+    }
+}
+
+int main() {
+    cin >> n;
+    cout << n << "\n";
+    col = (int *) malloc(n * sizeof(int));
+    diag1 = (int *) malloc(2 * n * sizeof(int));
+    diag2 = (int *) malloc(2 * n * sizeof(int));
+
+    search(0);
+
+    cout << cnt << "\n";
+
+    free(col);
+    free(diag1);
+    free(diag2);
+
+    return 0;
+}
