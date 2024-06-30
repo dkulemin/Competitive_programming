df0337daf16a522c62ce002638fdaec0d618817c
dkulemin <blablamaildk@gmail.com>
2024-06-30 11:12:50 +0500

    add tests

--- weird_algorithm/weird_algorithm.cpp
+++ weird_algorithm/weird_algorithm.cpp
@@ -15,15 +15,21 @@ void weird_algorithm(long long n) {
     cout << 1 << endl;
 }
 
-
 #ifdef _TEST
 #include "gtest/gtest.h"
 
-TEST(WeirdAlgorithm, EQ) {
+string capture_stdout(long long n) {
     testing::internal::CaptureStdout();
-    weird_algorithm(3);
-    string out = testing::internal::GetCapturedStdout();
-    EXPECT_EQ(out, "3 10 5 16 8 4 2 1\n");
+    weird_algorithm(n);
+    return testing::internal::GetCapturedStdout();
+}
+
+TEST(WeirdAlgorithm, EQ) {
+    EXPECT_EQ(capture_stdout(1), "1\n");
+    EXPECT_EQ(capture_stdout(2), "2 1\n");
+    EXPECT_EQ(capture_stdout(3), "3 10 5 16 8 4 2 1\n");
+    EXPECT_EQ(capture_stdout(5), "5 16 8 4 2 1\n");
+    EXPECT_EQ(capture_stdout(447), "447 1342 671 2014 1007 3022 1511 4534 2267 6802 3401 10204 5102 2551 7654 3827 11482 5741 17224 8612 4306 2153 6460 3230 1615 4846 2423 7270 3635 10906 5453 16360 8180 4090 2045 6136 3068 1534 767 2302 1151 3454 1727 5182 2591 7774 3887 11662 5831 17494 8747 26242 13121 39364 19682 9841 29524 14762 7381 22144 11072 5536 2768 1384 692 346 173 520 260 130 65 196 98 49 148 74 37 112 56 28 14 7 22 11 34 17 52 26 13 40 20 10 5 16 8 4 2 1\n");
 }
 
 int main(int argc, char **argv) {
